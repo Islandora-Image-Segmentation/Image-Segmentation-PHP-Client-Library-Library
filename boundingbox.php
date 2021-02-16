201ae8c627ac2cab757dee9f7d199c8c941742c3
Remah <rbadr@upei.ca>
2021-02-16 16:52:21 -0400

    Getters In BoundingBox

--- boundingbox.php
+++ boundingbox.php
@@ -11,9 +11,30 @@ public function __construct($upper_left_x,  $upper_left_y, $lower_right_x, $lowe
     $this->lower_right_x = $lower_right_x;
     $this->lower_right_y = $lower_right_y;
 }
+    public function getULX() {
+        return (float)$this->upper_left_x;
+    }
+
+    public function getULY() {
+        return (float)$this->upper_left_y;
+    }
+
+    public function getLRX() {
+        return (float)$this->lower_right_x;
+    }
+
+    public function getLRY() {
+        return (float)$this->lower_right_y;
+    }
 }
 
 $box = new BoundingBox(4.6, 5, 2, 3);
-//echo $box->upper_left_x;
+echo $box->getULX();
+echo "\n";
+echo $box->getULY();
+echo "\n";
+echo $box->getLRX();
+echo "\n";
+echo $box->getLRY();
 //echo "\n"
 ?>
