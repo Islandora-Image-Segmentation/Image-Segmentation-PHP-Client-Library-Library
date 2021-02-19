8f9334336503573d594c4ca30b2170ddf5eb9e5d
Kosay Jabre <kosay@thinkingbig.net>
2021-02-19 14:28:04 -0400

    Added SegmentationClient class

--- src/client.php
+++ src/client.php
@@ -1,25 +1,42 @@
 <?php
-use GuzzleHttp\Client;
-$API_URL = "http//localhost:8000/api";
-$client = new Client([
-    // Base URI is used with relative requests
-    'base_uri' => $API_URL,
-    // You can set any number of default request options.
-    'timeout'  => 30.0,
-]);
 
-function segmentImage($imageUrl)
+require_once "vendor/autoload.php";
+require_once "schemas.php";
+require_once "utils.php";
+use GuzzleHttp\Client;
+
+class SegmentationClient
 {
-    $client->request('POST', 'segment_image', ['json' => ['image' => $imageUrl]]);
-    $response =  $client->request('POST', 'segment_image', ['json' => ['image' => $imageUrl]]);
-    
-    if($response->getStatusCode() == 200)
-    {
-       return $response->getBody();
+    private Client $httpClient;
+
+    public function __construct(string $base_uri="http//localhost:8000/api", float $timeout=30.0) {
+        $this->httpClient = new Client(["base_uri" => $base_uri, "timeout" => $timeout]);
     }
-    else
+
+    public function segmentBase64(string $base64): SegmentationResponse
     {
-        new RequestException('Error Communicating with Server');
+
+    }
+
+
+    public function segmentUrl(string $url): SegmentationResponse
+    {
+
+    }
+
+    public function segmentFormdata(Imagick $image): SegmentationResponse
+    {
+
+    }
+
+    private function sendRequest(string $endpoint, array $json): string
+    {
+        $response = $this->$httpClient->request('POST', $endpoint, ['json' => $json]);
+        
+        if($response->getStatusCode() == 200)
+        {
+        return $response->getBody();
+        }
     }
 }
 ?>
