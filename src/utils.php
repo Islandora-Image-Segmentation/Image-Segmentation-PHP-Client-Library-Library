da3aac5f215a8fa8739358af41bcaa9013a5df4d
Kosay Jabre <kosay@thinkingbig.net>
2021-02-19 13:55:51 -0400

    Made crop image crop a clone instead of the original image

--- src/utils.php
+++ src/utils.php
@@ -1,15 +1,21 @@
 <?php
+require "schemas.php";
 
 
 function imageToBase64(Imagick $imageObj)
 { 
-    $return base64_encode($imageObj); 
+    return base64_encode($imageObj->getImageBlob()); 
 }
 
 
 function cropImage(Imagick $imageObj, BoundingBox $box): Imagick 
 {
-    $width = ($box->lower_right_x - $box->upper_left_x); 
-    $height =  ($box->lower_right_y - $box->upper_left_y); 
-    return cropImage($width, $height, $box->upper_left_x, $box->upper_left_y); // crop image
+    $width = $box->lower_right_x - $box->upper_left_x; 
+    $height = $box->lower_right_y - $box->upper_left_y; 
+    $imageClone = $imageObj->clone();
+    $imageClone->cropImage($width, $height, $box->upper_left_x, $box->upper_left_y);
+    return $imageClone;
+}
+
+
 ?>
