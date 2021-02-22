ed133cce95300b3c4a4a03f7f03f4fcc8fcff9cd
Kosay Jabre <kosay@thinkingbig.net>
2021-02-22 14:39:01 -0400

    Updated constructor to save base_uri and timeout

--- src/client.php
+++ src/client.php
@@ -8,14 +8,18 @@ use GuzzleHttp\Client;
 class SegmentationClient
 {
     private Client $httpClient;
+    private string $base_uri;
+    private float $timeout;
 
-    public function __construct(string $base_uri="http//localhost:8000/api", float $timeout=30.0) {
+    public function __construct(string $base_uri="localhost:8000/api/", float $timeout=30.0) {
+        $this->base_uri = $base_uri;
+        $this->time_out = $timeout;
         $this->httpClient = new Client(["base_uri" => $base_uri, "timeout" => $timeout]);
     }
 
     public function segmentBase64(string $base64): SegmentationResponse
     {
-
+        $response = $this->sendRequest("segment_base64", ["image_base64" => $base64]);
     }
 
 
@@ -24,19 +28,33 @@ class SegmentationClient
 
     }
 
-    public function segmentFormdata(Imagick $image): SegmentationResponse
+    public function segmentImagick(Imagick $image): SegmentationResponse
     {
+        
+    }
+
+    public function segmentFile(string $filepath): SegmentationResponse
+    {
+        
+        // 'multipart' => [
+        //     [
+        //         'name'     => 'FileContents',
+        //         'contents' => file_get_contents($path . $name),
+        //         'filename' => $name
+        //     ],
+        //     [
+        //         'name'     => 'FileInfo',
+        //         'contents' => json_encode($fileinfo)
+        //     ]
+        // ],
 
     }
 
     private function sendRequest(string $endpoint, array $json): string
     {
-        $response = $this->$httpClient->request('POST', $endpoint, ['json' => $json]);
-        
-        if($response->getStatusCode() == 200)
-        {
+        $response = $this->httpClient->request('POST', $this->base_uri . $endpoint, ['json' => $json]);
+        // should convert to SegmentationResponse here
         return $response->getBody();
-        }
     }
 }
 ?>
