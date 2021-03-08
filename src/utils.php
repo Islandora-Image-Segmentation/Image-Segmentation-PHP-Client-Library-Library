76f0cfde29a527a97762914fe7ba03802127d6c5
Space Duck Devourer of Worlds <alexandergcairns@gmail.com>
2021-03-08 12:48:04 -0400

    Fix bugs

--- src/utils.php
+++ src/utils.php
@@ -2,22 +2,24 @@
 namespace NewspaperNavigator;
 
 function imageToBase64(Imagick $imageObj): string
-{ 
-    return base64_encode($imageObj->getImageBlob()); 
+{
+    return base64_encode($imageObj->getImageBlob());
 }
 
 
-function cropImage(Imagick $imageObj, BoundingBox $box): Imagick 
+function cropImage(\Imagick $imageObj, BoundingBox $box): \Imagick
 {
-    $width = ($box->lower_right_x - $box->upper_left_x) * $imageObg->getImageWidth(); 
-    $height = ($box->lower_right_y - $box->upper_left_y) * $imageObg->getImageHeight(); 
-    $imageClone = $imageObj->clone();
-    $imageClone->cropImage($width, 
-                        $height, 
-                        $box->upper_left_x * $imageObg->getImageWidth(), 
-                        $box->upper_left_y * $imageObg->getImageHeight()
+    $width = ($box->lower_right_x - $box->upper_left_x) * $imageObj->getImageWidth();
+    $height = ($box->lower_right_y - $box->upper_left_y) * $imageObj->getImageHeight();
+    $imageClone = clone $imageObj;
+    $imageClone->cropImage($width,
+                        $height,
+                        $box->upper_left_x * $imageObj->getImageWidth(),
+                        $box->upper_left_y * $imageObj->getImageHeight()
                         );
+    $imageClone->setImagePage($width, $height, 0, 0);
     return $imageClone;
 }
+
 
 ?>
